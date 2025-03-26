654e00cae8f7c3604c8108723cfe871ad79693f8
Kirill-i-v <ivanchukkiril7a@gmail.com>
2025-03-26 18:53:40 +0200

    Homework 19

--- config/settings.py
+++ config/settings.py
@@ -82,9 +82,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "catering",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "database",
+        "PORT": "5432",
     }
 }
 
